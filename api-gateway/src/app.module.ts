cb39a071e79c52fd10c18f642cc0187f3691e951
Ericlys Moreira <ericlysm@gmail.com>
2026-02-17 09:51:54 -0300

    feat: add log middleware

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,24 +1,35 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  type MiddlewareConsumer,
+  type NestModule,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ProxyModule } from './proxy/proxy.module';
+import { MiddlewareModule } from './middleware/middleware.module';
+import { LoggingMiddleware } from './middleware/logging/logging.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
     }),
     ThrottlerModule.forRoot([
       {
         ttl: 60000, // 1 min
         limit: 100, // 100 req p minute
-      }
+      },
     ]),
-    ProxyModule
+    ProxyModule,
+    MiddlewareModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
